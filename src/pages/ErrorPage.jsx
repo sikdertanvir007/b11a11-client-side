38b10144fb164260cd77e669b9864ae6205efe8c
unknown <mehrabhasantanvir@gmail.com>
2025-08-10 14:55:12 +0600

    errorpage updated

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -8,10 +8,10 @@ const ErrorPage = () => {
             <Helmet> <title> Error</title></Helmet>
              <div className='px-3 md:px-15 lg:px-25 xl:px-30'>
              <div className='min-h-screen flex flex-col items-center justify-center text-center gap-y-6 '>
-                <img className='w-100 rounded-xl shadow-xl' src="https://cdn.dribbble.com/userupload/6038198/file/original-827b370e4e255882d5f00013565a2cc6.png?resize=1024x768&vertical=center" alt="" />
+                <img className='w-150 rounded-xl shadow-lg' src="https://media.istockphoto.com/id/1341577422/vector/people-work-in-warehouse-storage-logistic-service-set-workers-carry-cardboard-boxes.jpg?s=612x612&w=0&k=20&c=PUSq0pnLWG_K89rLRXX4BfcnGkq7FpoKbDSj1xQZ_ek=" alt="" />
                 <p className='text-red-500 font-bold text-3xl'>404- Page Not Found</p>
                 <p className='text-xl'>Oops! The page you're looking for doesn't exist</p>
-                <Link to="/"><button  className='btn bg-green-500 hover:bg-green-700 text-white '>Go Back Home</button> </Link>
+                <Link to="/"><button  className='btn bg-orange-500 hover:bg-orange-700 text-white '>Go Back Home</button> </Link>
             </div>
         </div>
         </div>
